2a08ea11433f604c80a87204147418206a45cb4f
paskanov <v.paskanov@gmail.com>
2022-10-21 11:33:22 +0300

    Change css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,28 @@ import s from './Dialogs.module.css';
 
 export const Dialogs =() => {
     return (
-        <div>
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className="dialog">
+                    Trent
+                </div>
+                <div className="dialog">
+                    Virgil
+                </div>
+                <div className="dialog">
+                    Mohamed
+                </div>
+                <div className="dialog">
+                    Darwin
+                </div>
+                <div className="dialog">
+                    Roberto
+                </div>
+
+            </div>
+            <div className="messages">
+
+            </div>
             Dialogs
         </div>
     )
