72b4779ca4f5014ca9cac9cdd20f600808fd955d
paskanov <v.paskanov@gmail.com>
2022-10-25 11:20:54 +0300

    Add Massives -24

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,27 +3,33 @@ import s from './Myposts.module.css'
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+    let myPostsData = [
+        {id: 1, like: "5", message: "Hello, how are you?"},
+        {id: 2, like: "10", message: "This is my first post)?"},
+
+    ]
+
     return (
-    <div className={s.postsBlock}>
-        <h3>My posts</h3>
-        <div>
-            <textarea></textarea>
-        </div>
-        <div>
-                        <button>Add post</button>
-        </div>
-    <div>
-        New posts
-    </div>
-    <div className={s.posts}>
-        <div className={s.item}>
-            <Post message='Hello, how are you?' like='5' />
-            <Post message='This is my first post)?' like='10'/>
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>Add post</button>
+            </div>
+            <div>
+                New posts
+            </div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                    <Post message={myPostsData[0].message} like={myPostsData[0].like}/>
+                    <Post message={myPostsData[1].message} like={myPostsData[1].like}/>
 
 
-    </div>
-</div>
-    </div>
-)
+                </div>
+            </div>
+        </div>
+    )
 }
 
