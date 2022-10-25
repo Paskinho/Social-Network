af5eb6fb3742791c103ad93e645d640a24f2cb90
paskanov <v.paskanov@gmail.com>
2022-10-25 17:12:30 +0300

    27 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
 import {Settings} from "./components/Settings/Settings";
 
+
+
+
 const App =() => {
   return (
       <BrowserRouter>
@@ -17,11 +20,15 @@ const App =() => {
      <Header />
       <Navbar />
     <div className = "app-wrapper-content">
-        <Route path={'/dialogs'} component={Dialogs} />
-        <Route path={'/profile'} component={Profile}/>
-        <Route path={'/news'} component={News}/>
-        <Route path={'/music'} component={Music}/>
-        <Route path={'/settings'} component={Settings}/>
+
+        <Route path={'/dialogs'} render={()=><Dialogs/> }/>
+        <Route path={'/profile'} render={()=><Profile/>}/>
+        <Route path={'/news'} render={()=><News/>}/>
+        <Route path={'/music'} render={()=><Music/>}/>
+        <Route path={'/settings'} render={()=><Settings/>}/>
+
+
+
     </div>
 
     </div>
