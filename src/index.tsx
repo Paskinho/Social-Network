18b196dd0c8c6bafa876d0de0f96a2f6fac9a91d
paskanov <v.paskanov@gmail.com>
2022-11-14 16:25:37 +0300

    Delete render.tsx lesson 35

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import './index.css';
 import {state} from "./Redux/state";
-import {rerenderEntireTree} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {addPost, RootStateType, updateNewPostText} from "./Redux/state";
+import {BrowserRouter} from "react-router-dom";
+
+
+
+export const rerenderEntireTree = (state: RootStateType) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+
+}
+
+
 
 rerenderEntireTree(state)
 
