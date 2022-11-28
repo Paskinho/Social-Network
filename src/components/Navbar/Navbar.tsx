2716be068961970ac10a2f0b9a10f63e713e6a96
paskanov <v.paskanov@gmail.com>
2022-11-28 11:38:43 +0300

    React router dom Params(style)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import s from './Navbar.module.css'
 import {NavLink} from "react-router-dom";
 
-
 export const Navbar = () => {
     return <nav className={s.nav}>
         <div className={s.item}>
