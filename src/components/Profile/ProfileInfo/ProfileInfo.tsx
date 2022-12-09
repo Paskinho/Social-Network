77d5bcd0e52f7a3951520ea4cd93516d835ec775
paskanov <v.paskanov@gmail.com>
2022-12-09 15:53:36 +0300

    Global changed CSS after 44 lesson

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -8,7 +8,8 @@ export const ProfileInfo = () => {
     return (
         <div>
         <div className={s.img}>
-            <img src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"/>
+            <img src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"
+            alt=''/>
         </div>
     <div className={s.descriptionBlock}>
         ava+description
