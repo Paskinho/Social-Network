1870e96d46c98ee2f04ccd661556fae6a8ceb0b4
paskanov <v.paskanov@gmail.com>
2022-12-12 16:55:18 +0300

    end of 45 lesson (typezitation)

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -2,14 +2,17 @@ import {combineReducers, createStore} from "redux"
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 
-let reducers = combineReducers({
+let rootReducer = combineReducers({
     profileReducer, //profilePage: profileReducer
     dialogsReducer,
 // sidebarReducer
 })
 
+
+export type AppStateType = ReturnType<typeof rootReducer>
+
 export type StoreType = typeof store
 
-let store = createStore(reducers)
+const store = createStore(rootReducer)
 
 export default store;
