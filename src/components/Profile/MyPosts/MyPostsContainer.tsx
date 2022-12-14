1bf8ac05a898012694ef478814d9327632dbc9e7
paskanov <v.paskanov@gmail.com>
2022-12-14 14:18:53 +0300

    end of 45 lesson (some refactoring)

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -3,21 +3,21 @@ import {MyPosts} from "./MyPosts";
 import {StoreType} from "../../../redux/redux-store";
 import {addPostCreator, updateNewPostTextCreator} from "../../../redux/profile-reducer";
 import {connect} from "react-redux";
-import {RootStateType} from "../../../redux/store";
+import {PostDataType, RootStateType} from "../../../redux/store";
 import {addMessageCreator, onMessagePostCreator} from "../../../redux/dialogs-reducer";
 
-//
-// type MapStatePropsType = {
-//     posts: string
-//     postText: string
-// }
+
+type MapStatePropsType = {
+    posts: PostDataType[]
+    postText: string
+}
 
 
 type MessageType = {
     store: StoreType
 }
 
-const mapStateToProps = (state: RootStateType) => { // : MapStatePropsType
+const mapStateToProps = (state: RootStateType) : MapStatePropsType => {
     return {
         posts: state.profilePage.postData,
         postText: state.profilePage.newPostText,
