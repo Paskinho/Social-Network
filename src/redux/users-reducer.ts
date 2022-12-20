bd225f7a8e84e824dab5b61b0306eb6c0dcfca24
paskanov <v.paskanov@gmail.com>
2022-12-20 17:04:30 +0300

    49 lesson small refactoring (почему-то не было uuid)

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,16 +1,21 @@
+import {v1} from "uuid";
 
 export type usersPageType = {
     users: Array<UsersType>
 }
 
 type UsersType = {
-    id: number,
+    id: string,
     followed: boolean,
     fullName: string,
     status: string,
-    location:
-        {city: string,
-            country: string}
+    location: LocationType
+}
+
+type LocationType = {
+    city: string,
+    country: string
+
 }
 
 const FOLLOW = "FOLLOW";
@@ -28,30 +33,50 @@ const initialState: any = {
 
 
     users: [
-        {id: 1, followed: true, fullName: 'Steven', status: 'Im a former Liverpool player and captain', location: {city: 'Liverpool', country: 'England'}},
-        {id: 2, followed: true, fullName: 'Kenny', status: 'Im a former Liverpool player and best of all time maybe', location: {city: 'Glasgow', country: 'Scotland'}},
-        {id: 3, followed: false, fullName: 'Luis', status: 'Im a former Liverpool player and owner golden boot in season 13/14', location: {city: 'Montevideo', country: 'Uruguay'}}
+        {
+            id: v1(),
+            followed: true,
+            fullName: 'Steven',
+            status: 'Im a former Liverpool player and captain',
+            location: {city: 'Liverpool', country: 'England'}
+        },
+        {
+            id: v1(),
+            followed: true,
+            fullName: 'Kenny',
+            status: 'Im a former Liverpool player and best of all time maybe',
+            location: {city: 'Glasgow', country: 'Scotland'}
+        },
+        {
+            id: v1(),
+            followed: false,
+            fullName: 'Luis',
+            status: 'Im a former Liverpool player and owner golden boot in season 13/14',
+            location: {city: 'Montevideo', country: 'Uruguay'}
+        }
     ]
 }
 
 export type InitialStateType = typeof initialState
 
-export const usersReducer = (state =initialState , action: ActionsTypes)=> {
+export const usersReducer = (state = initialState, action: ActionsTypes) => {
 
     switch (action.type) {
         case FOLLOW:
-    return {...state,
-    users: state.users.map( (u:any) => {
-        if (u.id === action.userId) {
-            return {...u, followed: true}
-        }
-        return u;
-        }
-    )
-    }
+            return {
+                ...state,
+                users: state.users.map((u: any) => {
+                        if (u.id === action.userId) {
+                            return {...u, followed: true}
+                        }
+                        return u;
+                    }
+                )
+            }
         case UNFOLLOW:
-            return {...state,
-                users: state.users.map( (u:any) => {
+            return {
+                ...state,
+                users: state.users.map((u: any) => {
                         if (u.id === action.userId) {
                             return {...u, followed: false}
                         }
@@ -64,13 +89,13 @@ export const usersReducer = (state =initialState , action: ActionsTypes)=> {
                 ...state, users: [...state.users, ...action.users]
             }
 
-        default: return state
+        default:
+            return state
     }
 
 }
 
 
-
-export const followAC =  (userId: string) => ({type: FOLLOW, userId})
-export const unfollowAC =  (userId: string) => ({type: UNFOLLOW, userId})
-export const setUsersAC =  (users:[]) => ({type: UNFOLLOW, users})
+export const followAC = (userId: string) => ({type: FOLLOW, userId})
+export const unfollowAC = (userId: string) => ({type: UNFOLLOW, userId})
+export const setUsersAC = (users: []) => ({type: UNFOLLOW, users})
