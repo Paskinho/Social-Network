69f90c93c936e22d62374f6bf44f561de96d2467
paskanov <v.paskanov@gmail.com>
2022-12-26 12:39:30 +0300

    change css for main image in profile

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -6,9 +6,8 @@ import s from './ProfileInfo.module.css'
 export const ProfileInfo: React.FC = () => {
     return (
         <div>
-        <div className={s.img}>
-            <img src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"
-            alt=''/>
+        <div >
+            <img className={s.img} src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"/>
         </div>
     <div className={s.descriptionBlock}>
         ava+description
