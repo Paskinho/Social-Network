8e07dc9b78f1a964fb147b4b18a01d9f53f98143
paskanov <v.paskanov@gmail.com>
2022-12-26 17:03:20 +0300

    55 lesson add url for axios

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,7 +13,7 @@ export class Users extends Component<UsersPropsType> {
 
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}$count=${this.props.pageSize}`).then((response) => {
             this.props.setUsers(response.data.items)
         })
 
@@ -24,7 +24,7 @@ export class Users extends Component<UsersPropsType> {
     render() {
         // const filteredUser = this.props.usersPage.users.filter((u, i) => i < this.props.usersPage.count)
 
-        const  pagesCount = this.props.totalUsersCount / this.props.pageSize
+        const  pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
 
         const pages = [];
         for (let i= 1; i <= pagesCount; i++) {
