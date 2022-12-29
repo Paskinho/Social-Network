a368bfc832de22c8e8fc20575d95b87daa1170e9
paskanov <v.paskanov@gmail.com>
2022-12-29 15:31:02 +0300

    59 lesson. Add ProfileContainer.tsx

---
+++ src/components/Profile/ProfileContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
+import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
+import {Profile} from "./Profile";
+
+
+
+
+
+class ProfileContainer extends React.Component<any , any>{
+    render () {
+    return (
+        <Profile/>
+    )
+    }
+}
