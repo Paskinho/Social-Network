d287da7f0ed467f98bb88b356bf70b8806abae18
paskanov <v.paskanov@gmail.com>
2023-01-20 11:43:51 +0300

    62 lesson. Add axios in button in Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,57 +3,73 @@ import s from "./Users.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {InitialStateType} from "../../redux/users-reducer";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 
 type UsersType = {
     totalUsersCount: number
     pageSize: number
     currentPage: number
-    unfollow: (userId: string)=> void
-    follow: (userId: string)=> void
-    onPageChanged: (p:number)=> void
+    unfollow: (userId: string) => void
+    follow: (userId: string) => void
+    onPageChanged: (p: number) => void
     usersPage: InitialStateType
 }
 
+export const Users = (props: UsersType) => {
 
-export const Users = (props: UsersType)=> {
-
-    const  pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
     const pages = [];
-    for (let i= 1; i <= pagesCount; i++) {
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
 
-    // @ts-ignore
     return <div>
-            <div>
-                <div>
-                    {pages.map(p => {
-                            return <span className={props.currentPage === p ? s.selectedPage : ""}
-                            onClick={(e)=> {props.onPageChanged(p)}}>{p}</span>
-                        })}
-
-        </div>
-        </div>
-    {
-     props.usersPage.users.map ((u:any) => <div> key={u.id}
-        <span>
         <div>
-            <NavLink to={'/profile/'+ u.id}>
-            <img src ={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
+            <div>
+                {pages.map(p => {
+                    return <span className={props.currentPage === p ? s.selectedPage : ""}
+                                 onClick={(e) => {
+                                     props.onPageChanged(p)
+                                 }}>{p}</span>
+                })}
+
+            </div>
+        </div>
+        {
+            props.usersPage.users.map((u: any) => <div> key={u.id}
+                <span>
+        <div>
+            <NavLink to={'/profile/' + u.id}>
+            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto}/>
                 </NavLink>
     </div>
     <div>
     {u.followed ?
-            <button onClick={()=> {props.unfollow(u.id)}}>UnFollow</button>
-    : <button onClick={()=> {props.follow(u.id)}}>Follow</button>
+        <button onClick={() => {
+
+            props.unfollow(u.id)}}>UnFollow</button>
+
+
+        : <button onClick={() => {
+
+            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                withCredentials: true
+            }).then((response) => {
+                this.props.toggleIsFetching(false)
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+
+
+            props.follow(u.id)}}>Follow</button>
 
     }
         </div>
         </span>
-        <span>
+                <span>
         <span>
             <div>{u.name}</div>
         <div>{u.status}</div>
@@ -64,7 +80,7 @@ export const Users = (props: UsersType)=> {
         </span>
         </span>
 
-        </div>)
-    }
+            </div>)
+        }
     </div>
 }
