4eb79438a815539bd2cf5b2afa2b12e16701e20b
paskanov <v.paskanov@gmail.com>
2023-02-24 14:17:14 +0300

    73 lesson. Add API for status

--- src/api/api.ts
+++ src/api/api.ts
@@ -35,9 +35,16 @@ export const profileAPI = {
 
     getProfile(userId: string) {
         return instance.get(`profile/` + userId);
+    },
+    getStatus(userId: string) {
+        return instance.get(`status/` + userId);
+    },
+    updateStatus(status: string) {
+        return instance.get(`status/`, {status: status});
     }
-}
 
+
+}
 
 
 export const authAPI = {
