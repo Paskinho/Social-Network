144b4cf2131b3cee61755a0f09b7b5d680d8e93e
paskanov <v.paskanov@gmail.com>
2023-03-13 16:52:31 +0300

    78 lesson. Error span

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -4,8 +4,11 @@ import s from "../FormsControls/FormsControls.module.css";
 
 export const TextArea = (input: any, meta: any, ...props: any) => {
     return (
-    <div className={s.formControl}>
+    <div className={s.formControl + " " + s.error}>
+        <div>
         <textarea {...input} {...props}/>
+        </div>
+        <span>{"Some error"}</span>
     </div>
     )
 }
