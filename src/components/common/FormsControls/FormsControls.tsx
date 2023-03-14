bf91576658795f7e1d9aa8af71153a99b2d430f8
paskanov <v.paskanov@gmail.com>
2023-03-14 12:48:03 +0300

    77 lesson. Refactoring FormsControls.tsx (add formControl)

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import s from "../FormsControls/FormsControls.module.css";
 
-export const TextArea = (input: any, meta: any, ...props: any) => {
-    const hasError = meta.error && meta.touched;
-    return (
-    <div className={s.formControl + " " + (hasError ? s.error : + "")}>
-        <div>
-        <textarea {...input} {...props}/>
-        </div>
-        {hasError && <span>{meta.error}</span>}
-    </div>
-    )
-}
 
-export const Input = (input: any, meta: any, ...props: any) => {
+const FormControl = (input: any, meta: any, child: any, ...props: any) => {
     const hasError = meta.error && meta.touched;
     return (
         <div className={s.formControl + " " + (hasError ? s.error : + "")}>
             <div>
-                <textarea {...input} {...props}/>
+                {props.child}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
+}
+
+export const TextArea = (props: any) => {
+    return <FormControl {...props}>
+        <textarea {...props.input} {...props}/>
+    </FormControl>
+}
+
+export const Input = (input: any, meta: any, ...props: any) => {
+    return <FormControl {...props}>
+        <input {...props.input} {...props}/>
+    </FormControl>
+
 }
