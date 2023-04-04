77e3e2ddfb9a3a3751391483a9f9dc77671a6799
paskanov <v.paskanov@gmail.com>
2023-04-04 11:54:22 +0300

    84 lesson. Create setStatus and create functions for ProfileStatusWithHooks.tsx

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -9,23 +9,34 @@ type ProfileStatusType = {
 
 export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
-    let [editMode, setEditMode] = useState(false)
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
 
     const activateEditMode = () => {
         setEditMode(true)
+    }
+
+    const deActivateMode = () => {
+        setEditMode(false)
+        //props.updateStatus(state.status);
+    }
+
+
+    const onStatusChange = (e: any) => {
+        setStatus(e.currentTarget.value)
     }
 
     return (
         <div>
             { !editMode &&
                 <div>
-                    <span onDoubleClick={activateEditMode}>{props.status}"------"</span>
+                    <span onDoubleClick={activateEditMode}>{props.status ||"------"}</span>
                 </div>
             }
             {
                 editMode &&
                 <div>
-                    <input autoFocus={true}/>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={deActivateMode}/>
                 </div>
             }
         </div>
