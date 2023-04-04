5aac5be36a31cf767e91f34546431d9a3444c52b
paskanov <v.paskanov@gmail.com>
2023-04-04 17:08:44 +0300

    85 lesson. Refactoring with Music.tsx

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -6,6 +6,15 @@ export const Music =() => {
     return (
         <div>
             Music
+            <div>
+            <span> Kanye West - Amazing</span>
+                <div>
+            <span> P Money - This year </span>
+                </div>
+                <div>
+            <span> System of a down - Aerials </span>
+                </div>
+            </div>
         </div>
     )
 }
