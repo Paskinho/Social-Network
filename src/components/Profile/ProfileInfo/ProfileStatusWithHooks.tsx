b22ef272abb900f0412c443514e1d767353f3ba3
paskanov <v.paskanov@gmail.com>
2023-04-04 12:41:55 +0300

    85 lesson. Create useEffect and finish lesson

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ProfileType} from "../ProfileContainer";
 
 type ProfileStatusType = {
@@ -9,8 +9,13 @@ type ProfileStatusType = {
 
 export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
-    const [editMode, setEditMode] = useState(false)
-    const [status, setStatus] = useState(props.status)
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+
+    useEffect(()=> {
+        setStatus(props.status)
+    }, [props.status])
+
 
     const activateEditMode = () => {
         setEditMode(true)
