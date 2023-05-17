dd29f3246743a66f62ab7625985a6d7607a5a9b2
paskanov <v.paskanov@gmail.com>
2023-05-17 12:40:22 +0300

    92 lesson. Some changes with test

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM, {unmountComponentAtNode} from "react-dom";
 import {SamuraiTSApp} from "./App";
 
-it('renders without crashing', function () {
-    const div = document.createElement('div')
-    ReactDOM.render(<SamuraiTSApp/>, div);
-    ReactDOM.unmountComponentAtNode(div)
-});
+// it('renders without crashing', () =>  {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<SamuraiTSApp/>, div);
+//     ReactDOM.unmountComponentAtNode(div)
+// });
