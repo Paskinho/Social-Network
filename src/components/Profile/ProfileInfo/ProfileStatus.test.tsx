3695d3e2ed18ca6d9f3a467dc3198fff86e283f3
paskanov <v.paskanov@gmail.com>
2023-05-31 12:38:37 +0300

    92/93 lesson. Create some testing

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM, {unmountComponentAtNode} from "react-dom";
+import {create} from "domain";
+import ProfileStatus from "./ProfileStatus";
+// import {create} from "react-test-renderer";
+
+
 
 // it('renders without crashing', () =>  {
 //     const div = document.createElement('div');
 //     ReactDOM.render(<SamuraiTSApp/>, div);
 //     ReactDOM.unmountComponentAtNode(div)
 // });
+
+
+describe('ProfileStatus component',()=> {
+    test('status from props should be in the state', ()=> {
+        const component = create(<ProfileStatus text='SUBSCRIBE TO BASIC'/>)
+        const instance = component.getInstance();
+        expect(instance.state.text).toBe('')
+    })
+})
