849a295637b5e58ed7bf52b70f591b81cae5f5fd
paskanov <v.paskanov@gmail.com>
2023-06-01 11:42:02 +0300

    Some css changes in Navbar.module.css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,37 +7,37 @@ export const Navbar = () => {
         <div className={s.item}>
             <NavLink to="/profile"
             style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>Profile</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to="/dialogs" style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>Message</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to="/users" style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>Users</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to="/news" style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>News</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to="/music" style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>Music</NavLink>
         </div>
         <div className={s.item}>
             <NavLink to="/settings" style={(params)=> {
-                return {color: params.isActive ? "red" : "darkred"}
+                return {color: params.isActive ? "red" : "darkcyan"}
             }
             }>Settings</NavLink>
         </div>
