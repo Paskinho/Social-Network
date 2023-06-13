3df6c9b78d257989298c277ce70e4fe3baa959b8
paskanov <v.paskanov@gmail.com>
2023-06-13 11:02:03 +0300

    96 lesson. Refactoring in ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,6 +4,7 @@ import {Preloader} from "../../common/Preloader/Preloader";
 import {ProfileType} from "../ProfileContainer";
 import {ProfileStatusWithHooks} from "./ProfileStatusWithHooks";
 import { Tilt } from 'react-tilt';
+import userPhoto from "../../../assets/images/user.png";
 
 
 
@@ -16,11 +17,11 @@ export const ProfileInfo: React.FC<ProfileType>= (props) => {
         <div>
         <div >
 
-            <img className={s.img} src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"/>
+            {/*<img className={s.img} src="https://backend.liverpoolfc.com/sites/default/files/styles/lg/public/2021-06/placeholder.jpg?itok=nhe1dpvk"/>*/}
 
             </div>
             <div>
-                {/*<img src={props.profile.photos.large}/>*/}
+                <img src={props.profile.photos.large || userPhoto} className={s.img}/>
                 <ProfileStatusWithHooks status={props.status} updateStatus={()=>{}}/>
             </div>
             <div>
