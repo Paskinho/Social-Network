419a4d7f09e4d325c846dda6628c9091ccdad3f1
paskanov <v.paskanov@gmail.com>
2023-06-20 11:04:33 +0300

    97 lesson. Some refactoring in ProfileInfo.tsx with data and form

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './ProfileInfo.module.css'
 import {Preloader} from "../../common/Preloader/Preloader";
 import {ProfileType} from "../ProfileContainer";
@@ -9,6 +9,9 @@ import {savePhoto, ServerProfileType} from "../../../redux/profile-reducer";
 
 
 export const ProfileInfo: React.FC<ProfileType> = (props) => {
+
+    const [editMode, setEditMode] = useState(false)
+
     if (props.profile) {
         return <Preloader/>
     }
@@ -31,7 +34,12 @@ export const ProfileInfo: React.FC<ProfileType> = (props) => {
             <div>
                 {/*<img src={props.profile.photos.large || userPhoto} className={s.img}/>*/}
                 {props.isOwner && <input className={s.fileButton} type={'file'} onChange={onMainPhotoSelected}/>}
-               <ProfileData profile={props.profile}/>
+                {editMode ? <ProfileDataForm profile={props.profile}/> : <ProfileData profile={props.profile}
+                isOwner={props.isOwner}
+                goToEditMode={()=> {
+                    return setEditMode(true)
+                }}
+                />}
                 <ProfileStatusWithHooks status={props.status} updateStatus={() => {
                 }}/>
             </div>
@@ -46,9 +54,39 @@ export const ProfileInfo: React.FC<ProfileType> = (props) => {
 
 type ProfileDataPropsType = {
     profile: ServerProfileType;
+    isOwner: boolean;
+    goToEditMode: () => void
 };
 
 const ProfileData: React.FC<ProfileDataPropsType> = (props) => {
+    return <div>
+        {props.isOwner && <div><button onClick={props.goToEditMode}>Edit</button> </div>}
+        <div className={s.status}>
+            <b>Full Name</b>: {props.profile.fullName}
+        </div>
+        <div className={s.status}>
+            <b>Looking for a job</b>: {props.profile.lookingForAJob ? 'yes' : 'no'}
+        </div>
+        {props.profile.lookingForAJob &&
+            <div className={s.status}>
+                <b>My professional skills</b>: {props.profile.lookingForAJobDescription}
+            </div>
+        }
+        <div className={s.status}>
+            <b>About me</b>: {props.profile.aboutMe}
+        </div>
+        {/*<div>*/}
+        {/*    <b>Contacts</b>: {Object.keys(props.profile.contacts).map(key => {*/}
+        {/*    return <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>*/}
+        {/*})}*/}
+        {/*</div>*/}
+    </div>
+}
+type ProfileDataFormPropsType = {
+    profile: ServerProfileType;
+};
+
+const ProfileDataForm: React.FC<ProfileDataFormPropsType> = (props) => {
     return <div>
         <div className={s.status}>
             <b>Full Name</b>: {props.profile.fullName}
@@ -71,7 +109,6 @@ const ProfileData: React.FC<ProfileDataPropsType> = (props) => {
         {/*</div>*/}
     </div>
 }
-
 
 const Contact = ({contactTitle, contactValue}: any) => {
     return <div>
