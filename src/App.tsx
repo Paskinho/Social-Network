4b5d7e7f5b5a326074a48013a6d114aaf0013010
paskanov <v.paskanov@gmail.com>
2023-07-17 10:26:57 +0300

    99 lesson. Add catchAllUnhandledErrors

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {News} from "./components/News/News";
 import {Settings} from "./components/Settings/Settings";
 // import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import  {withRouter} from "./components/Profile/ProfileContainer";
+import {withRouter} from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect, MapStateToProps, Provider} from "react-redux";
@@ -21,7 +21,6 @@ const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsCo
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 
-
 //
 // export  type appPropsType = {
 //     store: StoreType
@@ -31,15 +30,21 @@ const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileCo
 
 class App extends React.Component<any> {
 
+    catchAllUnhandledErrors = (promiseRejectionEvent) => {
+        alert(promiseRejectionEvent)
+    }
 
     componentDidMount() {
         // this.props.toggleIsFetching(true)
         this.props.initializeApp();
+        window.addEventListener('unhandledrejection', function (promiseRejectionEvent) {
+
+        })
     }
 
     render() {
 
-        if(!this.props.initialized) {
+        if (!this.props.initialized) {
             return <Preloader/>
         }
 
@@ -50,14 +55,14 @@ class App extends React.Component<any> {
                 <div className="app-wrapper-content">
                     <Routes>
                         <Route path='/login' element={<Login/>}/>
-                        <Route path='/profile' element={ <React.Suspense fallback={<Preloader/>}>
-                             <ProfileContainer/>
+                        <Route path='/profile' element={<React.Suspense fallback={<Preloader/>}>
+                            <ProfileContainer/>
                         </React.Suspense>}/>
                         <Route path='/dialogs' element={
-                                <React.Suspense fallback={<Preloader/>}>
+                            <React.Suspense fallback={<Preloader/>}>
                                 <DialogsContainer/>
-                                </React.Suspense>
-                           } // уточнить (добавить withSuspence вызовом функции)
+                            </React.Suspense>
+                        } // уточнить (добавить withSuspence вызовом функции)
                         />
                         <Route path='/users' element={<UsersContainer/>}/>
                         <Route path='/news' element={<News/>}/>
@@ -76,22 +81,21 @@ const mapStateToProps = (state: AppStateType) => ({
     initialized: state.app.isInitialized
 })
 
-type MapStateToPropsType =[
+type MapStateToPropsType = [
     initialized: boolean
 ]
 
 type AppType = MapStateToPropsType
 
 
-
-let AppContainer =  compose(withRouter, connect(mapStateToProps,{initializeApp}))(App);
+let AppContainer = compose(withRouter, connect(mapStateToProps, {initializeApp}))(App);
 
 export const SamuraiTSApp = (props: any) => {
     return (
-    <BrowserRouter>
-        <Provider store={store}>
-            <AppContainer/>
-        </Provider>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </BrowserRouter>
     )
 }
