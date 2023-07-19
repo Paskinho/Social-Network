a9fc50ac706aea86b4fc674c58339da1f2408cae
paskanov <v.paskanov@gmail.com>
2023-07-19 11:44:17 +0300

    99 lesson (after lesson). Some refactoring in reducers

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,6 +6,7 @@ import {ProfileType} from "./ProfileContainer";
 
 export const Profile: React.FC<ProfileType>= (props) => {
     return (
+
         <div>
             <ProfileInfo {...props}
             // isAuth={props.isAuth} // уточнить по isAuth
